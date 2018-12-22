07414262c5bb923631fceb22b771cb934b327b11
Abhishek Kathayat <abhishekkathayat@outlook.com>
2018-12-22 07:28:41 +0530

    Logical Changes

--- app/src/main/java/com/refshare/refshare/MainActivity.java
+++ app/src/main/java/com/refshare/refshare/MainActivity.java
@@ -1,13 +1,60 @@
 package com.refshare.refshare;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private Button backbutton;
+    private Button sharebutton;
+    private FirebaseAuth auth;
+    private TextView codeholder;
+    private String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        codeholder = (TextView)findViewById(R.id.textview_code);
+        backbutton = (Button)findViewById(R.id.button_signingout);
+        sharebutton = (Button)findViewById(R.id.button_share);
+
+        sharebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                code = codeholder.getText().toString().trim();
+                shareCode(code);
+            }
+        });
+
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    protected void shareCode(String code) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, code);
+        sendIntent.setType("text/plain");
+        sendIntent.setPackage("com.whatsapp");
+        startActivity(sendIntent);
+    }
+
+    protected void signOut() {
+        auth.signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 }
